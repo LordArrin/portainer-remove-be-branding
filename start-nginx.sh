5b8f94b18d2665dd8f43380a78baadc6729e164d
LordArrin <79581469+LordArrin@users.noreply.github.com>
2024-11-23 19:56:08 +0300

    Delete start-nginx.sh

--- start-nginx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export PORTAINER_HOSTNAME="${PORTAINER_HOSTNAME:-portainer}"
-export PORTAINER_PORT="${PORTAINER_PORT:-9000}"
-envsubst '$PORTAINER_HOSTNAME,$PORTAINER_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;'
