3cc0dcc38976993076e8a97009ffcdfc9260577e
LordArrin <79581469+LordArrin@users.noreply.github.com>
2025-03-18 13:56:12 +0300

    Update remove_be.js
    hide be ads from enviroment

--- remove_be.js
+++ remove_be.js
@@ -3,14 +3,13 @@
   const scriptLog = (message, prefix, groupLogs) => {
     const logArgs = [
       `%c${prefix ? prefix : '?'} %c[%cPortainer Proxy%c] %c${message}`,
-      "color: orange; font-weight: bold;", // Стиль для префикса
-      "color: gray; font-weight: bold;",   // Стиль для метки "[Portainer Proxy]"
-      "color: cyan; font-weight: bold;",   // Стиль для текста "Portainer Proxy"
-      "color: gray; font-weight: bold;",   // Стиль для текста сообщения
-      "color: white; font-weight: normal;",// Стиль для основного текста сообщения
+      "color: orange; font-weight: bold;",
+      "color: gray; font-weight: bold;",
+      "color: cyan; font-weight: bold;",
+      "color: gray; font-weight: bold;",
+      "color: white; font-weight: normal;",
     ];
 
-    // Если groupLogs задан, логируем в виде сгруппированного блока
     if (groupLogs) {
       console.groupCollapsed(...logArgs);
       groupLogs();
@@ -20,148 +19,151 @@
     }
   };
 
+  // Функция для принудительного скрытия целевого элемента
+  const removeDynamicElement = () => {
+    const elements = document.querySelectorAll('[class*="app-react-components-BoxSelector-BoxSelectorItem-module__business"]');
+    if (elements.length) {
+      elements.forEach(el => {
+        // Устанавливаем стиль непосредственно на элемент
+        el.style.setProperty("display", "none", "important");
+      });
+      scriptLog(`Принудительно скрыто ${elements.length} элемент(ов) с целевым классом`);
+    }
+  };
+
   // Функция для применения динамических стилей на основе текущего URL
   const applyStyles = () => {
     const url = window.location.href;
 
-    // Массив стилей, которые будут применяться в зависимости от паттернов URL
     const styles = [
+      {
+        // Блок для скрытия элементов, содержащих указанный класс
+        pattern: /.*/,
+        selectors: [
+          '[class*="app-react-components-BoxSelector-BoxSelectorItem-module__business"]'
+        ],
+      },
       {
         // Общие стили, применяемые ко всем страницам
         pattern: /.*/,
         selectors: [
-          "#sideview button:has(+ nav)",  // Кнопки с навигацией
-          '#sideview nav[aria-label="Settings"] li[aria-label="Users"] li:has(a[href="#!/roles"])', // Роли в настройках
-          '#sideview nav[aria-label="Settings"] li[aria-label="Authentication logs"]:has(a[href="#!/auth-logs"])', // Логирование аутентификации
-          '#sideview li[aria-label="Cluster"] li[aria-label="Security constraints"]', // Ограничения безопасности в кластере
+          "#sideview button:has(+ nav)",
+          '#sideview nav[aria-label="Settings"] li[aria-label="Users"] li:has(a[href="#!/roles"])',
+          '#sideview nav[aria-label="Settings"] li[aria-label="Authentication logs"]:has(a[href="#!/auth-logs"])',
+          '#sideview li[aria-label="Cluster"] li[aria-label="Security constraints"]',
         ],
       },
+      // Остальные блоки стилей, как были ранее...
       {
-        // Страница с настройками конечных точек
         pattern: /.*\/#!\/wizard\/endpoints($|\/create\?.*$)/,
         selectors: [
-          "#content-wrapper div.app-react-components-BoxSelector-BoxSelectorItem-module__business", // Стиль для создания новых конечных точек
+          "#content-wrapper div.app-react-components-BoxSelector-BoxSelectorItem-module__business",
         ],
       },
       {
-        // Страница с доступом для конкретной конечной точки
         pattern: /.*\/#!\/endpoints\/[0-9]+\/access($|\?.*$)/,
         selectors: [
-          "#content-wrapper div.form-group:has(be-feature-indicator)", // Стиль для групп, связанных с фичами
+          "#content-wrapper div.form-group:has(be-feature-indicator)",
         ],
       },
       {
-        // Страница с регистрами
         pattern: /.*\/#!\/registries.*/,
-        selectors: ["#content-wrapper a.be-indicator"], // Скрытие индикаторов на странице с регистрами
+        selectors: ["#content-wrapper a.be-indicator"],
       },
       {
-        // Страница с настройками
         pattern: /.*\/#!\/settings.*/,
         selectors: [
-          "#content-wrapper div.form-group:has(a.be-indicator)", // Скрытие групп с индикаторами
-          "#content-wrapper div.be-indicator-container", // Контейнеры с индикаторами
-          "#content-wrapper div.app-react-components-BoxSelector-BoxSelectorItem-module__business", // Боксы выбора
+          "#content-wrapper div.form-group:has(a.be-indicator)",
+          "#content-wrapper div.be-indicator-container",
+          "#content-wrapper div.app-react-components-BoxSelector-BoxSelectorItem-module__business",
         ],
       },
       {
-        // Страница с настройками аутентификации
         pattern: /.*\/#!\/settings\/auth.*/,
         selectors: [
-          "#content-wrapper div.app-react-components-BoxSelector-BoxSelectorItem-module__business", // Настройки LDAP
-          '#content-wrapper ldap-settings label[for="ldap_url"] button.limited-be', // Кнопка для настройки LDAP URL
-          "#content-wrapper ldap-settings ldap-custom-user-search > div.form-group div.col-sm-12:has(be-feature-indicator)", // Пользовательский поиск в LDAP
-          "#content-wrapper ldap-settings ldap-custom-group-search > rd-widget div.form-group:has(pr-icon[icon=\"'briefcase'\"])", // Группы пользователей в LDAP
-          "#content-wrapper ldap-settings ldap-custom-group-search > div.form-group div.col-sm-12:has(be-feature-indicator)", // Поиск групп в LDAP
-          "#content-wrapper ldap-settings ldap-custom-admin-group", // Группа администраторов в LDAP
-          "#content-wrapper ldap-settings ldap-settings-test-login", // Тестирование входа LDAP
-          "#content-wrapper oauth-settings div.form-group:has(a.be-indicator)", // OAuth настройки
+          "#content-wrapper div.app-react-components-BoxSelector-BoxSelectorItem-module__business",
+          '#content-wrapper ldap-settings label[for="ldap_url"] button.limited-be',
+          "#content-wrapper ldap-settings ldap-custom-user-search > div.form-group div.col-sm-12:has(be-feature-indicator)",
+          "#content-wrapper ldap-settings ldap-custom-group-search > rd-widget div.form-group:has(pr-icon[icon=\"'briefcase'\"])",
+          "#content-wrapper ldap-settings ldap-custom-group-search > div.form-group div.col-sm-12:has(be-feature-indicator)",
+          "#content-wrapper ldap-settings ldap-custom-admin-group",
+          "#content-wrapper ldap-settings ldap-settings-test-login",
+          "#content-wrapper oauth-settings div.form-group:has(a.be-indicator)",
         ],
       },
       {
-        // Страница с настройками стека Docker
         pattern: /.*\/#!\/[0-9]+\/docker\/stacks\/newstack($|.*$)/,
         selectors: [
-          "#content-wrapper div:has(> div.form-section-title a.be-indicator)", // Секции с индикаторами
-          "#content-wrapper div > div.form-section-title:has(a.be-indicator)", // Заголовки секций с индикаторами
-          "#content-wrapper tr:has(div.form-group a.be-indicator)", // Строки с индикаторами
+          "#content-wrapper div:has(> div.form-section-title a.be-indicator)",
+          "#content-wrapper div > div.form-section-title:has(a.be-indicator)",
+          "#content-wrapper tr:has(div.form-group a.be-indicator)",
         ],
       },
       {
-        // Страница с созданием нового контейнера
         pattern: /.*\/#!\/[0-9]+\/docker\/containers\/new($|.*$)/,
         selectors: [
-          "#content-wrapper div:has(> div.form-group a.be-indicator)", // Группы с индикаторами
+          "#content-wrapper div:has(> div.form-group a.be-indicator)",
         ],
       },
       {
-        // Страница с контейнерами Docker
         pattern: /.*\/#!\/[0-9]+\/docker\/containers\/[0-9a-fA-F]{64}.*/,
-        selectors: ["#content-wrapper tr:has(div.form-group a.be-indicator)"], // Строки с индикаторами
+        selectors: ["#content-wrapper tr:has(div.form-group a.be-indicator)"],
       },
       {
-        // Страница с конфигурацией хоста Docker
         pattern: /.*\/#!\/[0-9]+\/docker\/host\/feat-config.*/,
         selectors: [
-          "#content-wrapper div.form-group div.col-sm-12:has(a.be-indicator)", // Группы с индикаторами
-          "#content-wrapper div.form-section-title:has(+ div.form-group div.col-sm-12:only-child a.be-indicator)", // Заголовки с индикаторами
+          "#content-wrapper div.form-group div.col-sm-12:has(a.be-indicator)",
+          "#content-wrapper div.form-section-title:has(+ div.form-group div.col-sm-12:only-child a.be-indicator)",
         ],
       },
       {
-        // Страница с регистрами хоста Docker
         pattern: /.*\/#!\/[0-9]+\/docker\/host\/registries.*/,
-        selectors: ["#content-wrapper be-feature-indicator"], // Индикаторы фич
+        selectors: ["#content-wrapper be-feature-indicator"],
       },
       {
-        // Страница с настройками пула Kubernetes
         pattern: /.*\/#!\/[0-9]+\/kubernetes\/pools\/new($|.*$)/,
         selectors: [
-          "#content-wrapper div.form-group:has(label a.be-indicator)", // Группы с индикаторами
-          "#content-wrapper div.form-section-title:has(+ div.form-group + div.form-section-title + storage-class-switch a.be-indicator)", // Заголовки с индикаторами
-          "#content-wrapper div.form-section-title + div.form-group:has(+ div.form-section-title + storage-class-switch a.be-indicator)", // Заголовки секций
+          "#content-wrapper div.form-group:has(label a.be-indicator)",
+          "#content-wrapper div.form-section-title:has(+ div.form-group + div.form-section-title + storage-class-switch a.be-indicator)",
+          "#content-wrapper div.form-section-title + div.form-group:has(+ div.form-section-title + storage-class-switch a.be-indicator)",
         ],
       },
       {
-        // Страница с развертыванием Kubernetes
         pattern: /.*\/#!\/[0-9]+\/kubernetes\/deploy($|\?.*$)/,
         selectors: [
-          "#content-wrapper div.form-group:has(label a.be-indicator)", // Группы с индикаторами
+          "#content-wrapper div.form-group:has(label a.be-indicator)",
         ],
       },
       {
-        // Страница с конфигурацией кластера Kubernetes
         pattern: /.*\/#!\/[0-9]+\/kubernetes\/cluster\/configure.*/,
         selectors: [
-          "#content-wrapper div.form-section-title:has(+ div.form-group por-switch-field[name=\"'disableSysctlSettingForRegularUsers'\"] a.be-indicator)", // Настройки sysctl
-          "#content-wrapper div.form-section-title + div.form-group:has(por-switch-field[name=\"'disableSysctlSettingForRegularUsers'\"] a.be-indicator)", // Заголовки секций
-          "#content-wrapper div:has(> por-switch-field[name=\"'restrictStandardUserIngressW'\"] a.be-indicator)", // Переключатели конфигурации
-          "#content-wrapper div:has(+ div > por-switch-field[name=\"'resource-over-commit-switch'\"] a.be-indicator)", // Переключатели ресурсов
+          "#content-wrapper div.form-section-title:has(+ div.form-group por-switch-field[name=\"'disableSysctlSettingForRegularUsers'\"] a.be-indicator)",
+          "#content-wrapper div.form-section-title + div.form-group:has(por-switch-field[name=\"'disableSysctlSettingForRegularUsers'\"] a.be-indicator)",
+          "#content-wrapper div:has(> por-switch-field[name=\"'restrictStandardUserIngressW'\"] a.be-indicator)",
+          "#content-wrapper div:has(+ div > por-switch-field[name=\"'resource-over-commit-switch'\"] a.be-indicator)",
         ],
       },
     ];
 
-    const styleBlockId = "remove-be-style"; // ID блока стилей
+    const styleBlockId = "remove-be-style";
     let styleBlock = document.getElementById(styleBlockId);
 
-    // Если блок стилей не существует, создаем его
     if (!styleBlock) {
       styleBlock = document.createElement("style");
       styleBlock.id = styleBlockId;
       document.head.appendChild(styleBlock);
     }
 
-    const cssRules = []; // Массив для хранения CSS правил
+    const cssRules = [];
 
-    // Применяем стили в зависимости от текущего URL
     styles.forEach((style) => {
       if (style.pattern.test(url)) {
         style.selectors.forEach((selector) => {
-          cssRules.push(`${selector} { display: none !important; }`); // Скрытие элементов с помощью display: none
+          cssRules.push(`${selector} { display: none !important; }`);
         });
       }
     });
 
-    // Если есть правила для применения, выводим их в консоль
     if (cssRules.length) {
       scriptLog("Page state changed, applied dynamic styles", null, () => {
         console.log(cssRules.join("\n"));
@@ -170,11 +172,11 @@
       scriptLog("Page state changed, no styles to apply");
     }
 
-    // Применяем сформированные CSS правила
     styleBlock.innerHTML = cssRules.join("\n");
+    // Пробуем принудительно скрыть элемент
+    removeDynamicElement();
   };
 
-  // Создаем наблюдатель за изменениями DOM
   const observer = new MutationObserver(() => {
     applyStyles();
   });
@@ -184,12 +186,12 @@
     subtree: true,
   };
 
-  // Инициализация наблюдателя
   const initObserver = () => {
     observer.observe(document.body, config);
   };
 
-  // Применяем стили после загрузки контента и на изменение состояния истории
+  scriptLog("Using dynamic style application script with MutationObserver", "?");
+
   document.addEventListener("DOMContentLoaded", () => {
     applyStyles();
     initObserver();
